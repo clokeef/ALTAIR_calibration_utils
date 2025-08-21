ca641084d5a5184380b47fc7e193d8bba7e66f65
clokeef <clokeef@gmail.com>
2025-08-21 10:45:00 -0700

    Update mag_zero_est.py
    Updated documentation

--- mag_zero_est.py
+++ mag_zero_est.py
@@ -1,6 +1,4 @@
-#import the necessary packages
-
-#file handling and astropy helpers
+#import astropy table
 from astropy.table import Table
 
 from JG_Streaktools import *
@@ -8,9 +6,11 @@ from JG_Streaktools import *
 
 def build_corr_table(corr):
     """
-    puts together an Astropy table of the correlation files data
-    :param corr: correlation file from astrometry.net plate solution
-    :return: Astropy Table of the correlation data
+    builds an Astropy table of the astrometry.net correlation file data
+    Args:
+        corr: correlation file from astrometry.net plate solution
+
+    Returns: Astropy table of the correlation data
     """
     hd = corr[1].header
     data = corr[1].data
@@ -28,12 +28,18 @@ def build_corr_table(corr):
     table = Table(rows=data_rows, names=names)
     return table
 
-def get_magzero_est(corr,sim, file_path, file_name, read_in = False):
+def get_magzero_est(corr, sim, file_path, file_name, read_in = False):
     """
-    estimates the magnitude zero-point for an image given an astrometry.net corr file
-    :param corr: correlation file produced by astrometry.net plate solution
-    :param sim: background subbed streak_interface instance of the associated image
-    :return: median magnitude zero-point, array of calculated magnitude zero-points
+    estimates the magnitude zero-point for an image
+    Args:
+        corr: table or correlation file from astrometry.net
+        sim: background subtracted streak interface instance of the associated iamge
+        file_path: path to the working directory
+        file_name: file name to write the output table
+        read_in: boolean which indicates if the magzero estimate table should be read in and not recalculated
+
+    Returns: correlation table with an added magzero column and the median magnitude zero-point
+            this correlation table is then written out as a fits file with the median magnitude zero-point in the header, to be read in if needed
     """
     if read_in == True:
         table_fits  = fits.open(file_path + file_name)
